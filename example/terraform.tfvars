891b758eec0f310379f6949a43eafb16eecce1f9
japanjot15 <79826990+japanjot15@users.noreply.github.com>
2022-10-19 15:21:13 +0530

    files

---
+++ example/terraform.tfvars
@@ -0,0 +1,2 @@
+aws_region        = "eu-central-1"
+ec2_instance_type = "t2.micro"
---
+++ example/variable.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+ 
+variable "ec2_instance_type" {
+  description = "Instance type for EC2 instances"
+  type        = string
+}
